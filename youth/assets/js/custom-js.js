054772c1d5ba58ba5e5c340ad41d26396a070447
ehas <ehasahamed@gmail.com>
2024-04-22 13:30:38 +0530

    gallery

---
+++ youth/assets/js/custom-js.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {  
+   $(".read-more").click(function(){
+
+$(".list_1").hide();
+$(".list_2").show();     
+$(".list_3").hide();           
+    }); 
+    $(".read-more1").click(function(){
+
+        $(".list_1").hide();
+        $(".list_3").show();     
+        $(".list_2").hide();           
+            }); 
+    });  
